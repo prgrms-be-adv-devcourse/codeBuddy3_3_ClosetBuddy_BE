1b63b86a3b75de65d3849632536c349c6d5e2aee
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-01-08 22:23:12 +0900

    -[#8] 상품, 상점, 판매자 관리 서비스 개발 DTO 보강 작업

--- src/main/java/io/codebuddy/closetbuddy/catalog/domain/products/model/dto/ProductResponse.java
+++ src/main/java/io/codebuddy/closetbuddy/catalog/domain/products/model/dto/ProductResponse.java
@@ -1,11 +1,11 @@
 package io.codebuddy.closetbuddy.catalog.domain.products.model.dto;
 
-public record UpsertProductRequest(
+public record ProductResponse(
         Long productId,
         String productName,
         Long productPrice,
         int productStock,
         Long storeId
-        //추후 관심상품, 상품 설명, 상태등 추가예정
+        //추후 storeName 포함 가능
 ) {
 }
