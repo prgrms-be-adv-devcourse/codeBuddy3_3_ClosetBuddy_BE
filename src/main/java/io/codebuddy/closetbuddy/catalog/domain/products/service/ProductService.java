feb3ade223b672a23196faf5476c245e235d4bf8
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-01-08 16:50:03 +0900

    [#9] 상품 전체 조회 기능 개발 완료

--- src/main/java/io/codebuddy/closetbuddy/catalog/domain/products/service/ProductService.java
+++ src/main/java/io/codebuddy/closetbuddy/catalog/domain/products/service/ProductService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -26,5 +28,10 @@ public class ProductService {
                 .build();
 
         return productJpaRepository.save(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return productJpaRepository.findAll();
     }
 }
