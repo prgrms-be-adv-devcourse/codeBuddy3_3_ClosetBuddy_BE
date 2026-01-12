139f82f90925d6af283e1152bf27f6159798f50a
rksdu <rlaskdud444@gmail.com>
2026-01-12 12:19:52 +0900

    remove application.yml from git and ignore it

--- src/main/java/io/codebuddy/closetbuddy/domain/carts/Dto/CartItemUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package io.codebuddy.closetbuddy.domain.carts.Dto;
-
-import jakarta.validation.constraints.Min;
-
-public record CartItemUpdateDto(
-        @Min(value = 1, message = "최소 1개여야 합니다.")
-        Integer cartStock
-){
-}
