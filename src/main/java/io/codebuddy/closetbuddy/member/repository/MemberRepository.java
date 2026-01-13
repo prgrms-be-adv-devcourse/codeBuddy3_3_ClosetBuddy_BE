44551ee96d218fe8abc6fb38e0960ea7cb7178b6
SongEunKyung <108480766+siwol10@users.noreply.github.com>
2026-01-13 17:24:50 +0900

    [#15] delete member

--- src/main/java/io/codebuddy/closetbuddy/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package io.codebuddy.closetbuddy.member.repository;
-
-import io.codebuddy.closetbuddy.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
--- src/main/java/io/codebuddy/closetbuddy/member/role/Role.java
+++
@@ -1,5 +0,0 @@
-package io.codebuddy.closetbuddy.member.role;
-
-public enum Role {
-    ADMIN, MEMBER, GUEST
-}
